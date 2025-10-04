e52f8bf8ed5ddea81ef0c99ca8deff85c016138a
mohamed50mostafa <mohamedmostafakheder@outlook.com>
2025-10-04 22:01:40 +0300

    main

--- project/settings.py
+++ project/settings.py
@@ -129,14 +129,15 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
+from pathlib import Path
+import dj_database_url  
+# ⚡ إعدادات قاعدة البيانات - استبدل الكود القديم بهذا
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',  # للتطوير المحلي
+        conn_max_age=600
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
